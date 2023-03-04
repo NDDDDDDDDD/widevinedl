4f051233ef4c4eb501e736c01d817d4c55007839
NDDDDDDDDD <102587802+NDDDDDDDDD@users.noreply.github.com>
2023-03-04 18:25:47 +0000

    Update widevinedl.py
    optimized

--- widevinedl.py
+++ widevinedl.py
@@ -2,6 +2,9 @@ import subprocess
 from urllib.request import urlopen
 import re
 import os
+import base64
+import requests
+
 
 dir = r"C:\Users\46735\Documents\downey"  # this directory should include downey.exe
 mpd = input("enter MPD url: ")
@@ -13,9 +16,13 @@ cmdd = f"downey.exe --lic-server {licenseurl}"
 rc = subprocess.check_output(cmdd, cwd=dir)  # catches decryption key
 result = re.search(r"\B[a-z0-9]{16,}:[a-z0-9]{16,}", str(rc))
 decryption_key = result.group()
-os.system(f'N_m3u8DL-CLI_v3.0.2.exe {mpd}  --enableDelAfterDone --noMerge --enableMuxFastStart')  #downloading content in .mpd file
-filename = input("enter file name: ") # VERY important step, the file name is located at the top of the new entries in your terminal.
-os.system(fr'mp4decrypt.exe --key {decryption_key} C:\Users\46735\PycharmProjects\pythonProject\Downloads\{filename}.mp4 C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-video.mp4') # download will be located where you have your python file enter that path in all of these.
-os.system(fr'mp4decrypt.exe --key {decryption_key} C:\Users\46735\PycharmProjects\pythonProject\Downloads\{filename}(Audio).aac C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-audio.aac')
-name = input("enter final file name: ") # what the merged file will be called
-os.system(fr'mkvmerge.exe -o C:\Users\46735\Documents\downey\{name}.mkv C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-video.mp4 C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-audio.aac')
+name = input('enter final file name: ')
+os.system(f'N_m3u8DL-CLI_v3.0.2.exe {mpd} --enableMuxFastStart')  # downloading content in .mpd file
+mpd = mpd.split('/')
+
+os.system(
+    fr'mp4decrypt.exe --key {decryption_key} C:\Users\46735\PycharmProjects\pythonProject\Downloads\{mpd[-1]}.mp4 C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-video.mp4')  # download will be located where you have your python file enter that path in all of these.
+os.system(
+    fr'mp4decrypt.exe --key {decryption_key} C:\Users\46735\PycharmProjects\pythonProject\Downloads\{mpd[-1]}(Audio).aac C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-audio.aac')
+os.system(
+    fr'mkvmerge.exe -o C:\Users\46735\Documents\downey\{name}.mkv C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-video.mp4 C:\Users\46735\PycharmProjects\pythonProject\Downloads\decrypted-audio.aac')
